5f9283e1882ddee96e5ddd4eaac69685eac59608
AlvaroLC <alvarolopezcab21@gmail.com>
2021-11-22 11:40:00 +0100

    Devuelve listado de alertas con hilos

--- app/src/main/java/com/alvarolc/psp_playground/exercise_plagricola/presentation/AlertActivity.kt
+++ app/src/main/java/com/alvarolc/psp_playground/exercise_plagricola/presentation/AlertActivity.kt
@@ -5,23 +5,28 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.widget.TextView
+import androidx.activity.viewModels
 import com.alvarolc.psp_playground.R
 import com.alvarolc.psp_playground.exercise_plagricola.app.AlertRetrofitApiClient
 import com.alvarolc.psp_playground.exercise_plagricola.data.AlertDataRepository
 import com.alvarolc.psp_playground.exercise_plagricola.domain.GetAlertsUseCase
 import com.alvarolc.psp_playground.ut02.folder.data.Exercise02Activity
 import com.alvarolc.psp_playground.exercise_plagricola.data.AlertRemoteSource
+import kotlin.concurrent.thread
 
 class AlertActivity : AppCompatActivity() {
 
         private val TAG: String = AlertActivity::class.java.simpleName
         private val userRepository = AlertDataRepository(AlertRemoteSource(AlertRetrofitApiClient()))
+        //private val viewModel by viewModels<AlertViewModel>()
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            Thread {
+            //exampleCoroutines()
+            thread {
                 runRepository()
             }.start()
+
         }
 
         private fun runRepository() {
@@ -39,6 +44,10 @@ class AlertActivity : AppCompatActivity() {
             }
 
         }
+/**
+    private fun exampleCoroutines() {
+        viewModel.getUserViewModelScope()
+    }*/
 
     }
 
--- app/src/main/java/com/alvarolc/psp_playground/exercise_plagricola/presentation/AlertViewModel.kt
+++ app/src/main/java/com/alvarolc/psp_playground/exercise_plagricola/presentation/AlertViewModel.kt
@@ -1,8 +1,36 @@
 package com.alvarolc.psp_playground.exercise_plagricola.presentation
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.alvarolc.psp_playground.exercise_plagricola.app.AlertRetrofitApiClient
+import com.alvarolc.psp_playground.exercise_plagricola.data.AlertDataRepository
+import com.alvarolc.psp_playground.exercise_plagricola.data.AlertRemoteSource
 import com.alvarolc.psp_playground.exercise_plagricola.domain.GetAlertsUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class AlertViewModel(private val getAlertsUseCase: GetAlertsUseCase) : ViewModel() {
-    fun getAllAlerts() = getAlertsUseCase.execute()
+class AlertViewModel() : ViewModel() {
+
+    private val userRepository = AlertDataRepository(AlertRemoteSource(AlertRetrofitApiClient()))
+    private val TAG: String = AlertActivity::class.java.simpleName
+
+
+    fun getUserViewModelScope() {
+
+        viewModelScope.launch(Dispatchers.Main) {
+            //Visualizo el listado de alerts
+            val alerts = userRepository.getAlerts()
+            alerts.forEach { userApiModel ->
+                Log.d(TAG, "$userApiModel")
+            }
+
+            //Obtengo un usuario y visualizo el usuario
+            val alert = userRepository.getAlert("1671086")
+            alert?.run {
+                Log.d(TAG, "$this")
+            }
+        }
+
+    }
 }
